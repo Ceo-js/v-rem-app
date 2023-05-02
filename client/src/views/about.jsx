83ae48a84d2c31ec303ef9f2498db437187c9533
Ceo-Sammarco <noah.kd@gmail.com>
2023-05-02 20:55:43 -0300

    Ceo - ter 02 mai 2023 20:55:43 -03

---
+++ client/src/views/about.jsx
@@ -0,0 +1,26 @@
+
+
+import React from "react";
+import "./about.css";
+
+
+export default class About extends React.Component {
+    render() { document.title = "About"; return( <>
+        <galerry>
+            <t1>About</t1>
+            <hr />
+            <label>
+                <input type="text" placeholder="preço" />
+            </label>
+            <label>
+                <input type="text" placeholder="preço antigo" />
+            </label>
+            <label>
+                <input type="text" placeholder="desconto" />
+            </label>
+        </galerry>
+    </> ); }
+}
+
+
+// export default About;
--- server/server.js
+++ server/server.js
@@ -1,3 +1,65 @@
 
 
-const Lyh = "Lýh Oliveira Gangello";
+const 
+    ceo = require( "./src/utils/ceo" ),
+    doQuery = require( "./src/utils/queries" ),
+    express = require( "express" ),
+    app = express(),
+    mysql = require( "mysql2" ),
+    cors = require( "cors" ),
+    path = require( "path" ),
+    http = require( "http" ),
+    cookieParser = require( "cookie-parser" );
+    
+    require( "dotenv" ).config();
+
+
+app.set( "view engine", "ejs" );
+// app.set( "views", path.join( __dirname, "./src/pages" ) );
+app.set( "views", path.resolve( "src", "views" ) );
+
+app.use( express.json() );
+app.use( express.urlencoded( { extended: false } ) );
+app.use( cookieParser() );
+
+app.use( express.static( path.join( __dirname, "src/public" ) ) );
+
+app.use( cors() );
+
+const db = mysql.createPool( {
+    host: process.env.DB_HOST,
+    // port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+} );
+
+
+
+/* ==[ routes ]======================================== */
+const routes = {
+    users: require( "./src/routes/users" ),
+    index: require( "./src/routes/index" ),
+};
+app.use( "/", routes.index );
+app.use( "/users", routes.users );
+
+app.use( (req, res, next ) => {
+    return res.status( 404 ).render( "page404" );
+} );
+/* ==[ server listener ]======================================== */
+// const serverListener = 
+//     app.listen( process.env.DB_PORT || serverGate, () => {
+//         console.warn( 
+//             `> server: http://${ process.env.DB_HOST }:${ serverListener.address().port }` );
+// } );
+
+// app.listen( process.env.DB_PORT || serverGate );
+const port = process.env.DB_PORT || serverGate;
+
+http.createServer( app ).listen( port, () => {
+    console.warn( 
+        `> server: http://${ process.env.DB_HOST }:${ port }` );
+} );
+
+module.exports = app;
